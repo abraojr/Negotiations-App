4dadd0779675c60f366c7481772e85ccf5cb3876
Abrão Astério Junior <abraoastjr@gmail.com>
2021-10-04 22:29:24 -0300

    Project created

---
+++ server-api/app/routes/index.js
@@ -0,0 +1,7 @@
+/* Simplistic code, just to provide the service for the application */
+
+var api = require("../api");
+
+module.exports = function (app) {
+  app.route("/data").get(api.data);
+};
---
+++ server-api/config/express.js
@@ -0,0 +1,21 @@
+/* Simplistic code, just to provide the service for the application */
+
+var express = require("express"),
+  app = express(),
+  routes = require("../app/routes"),
+  path = require("path");
+
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+routes(app);
+
+module.exports = app;
