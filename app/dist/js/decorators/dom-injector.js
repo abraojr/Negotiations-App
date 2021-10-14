ed2f642beb0817e5cb372907016c4b8b20ce53f4
Abrão Astério Junior <abraoastjr@gmail.com>
2021-10-13 22:45:11 -0300

    Applying cache decorator copy

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -1,7 +1,12 @@
 export function domInjector(selector) {
     return function (target, propertyKey) {
+        console.log(`Modifying prototype ${target.constructor.name} and adding getter for the ${propertyKey} property`);
+        let element;
         const getter = function () {
-            const element = document.querySelector(selector);
+            if (!element) {
+                element = document.querySelector(selector);
+                console.log(`Fetching DOM element with ${selector} selector to inject into ${propertyKey}`);
+            }
             return element;
         };
         Object.defineProperty(target, propertyKey, {
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,8 +1,15 @@
 export function domInjector(selector: string) {
     return function (target: any, propertyKey: string) {
+        console.log(`Modifying prototype ${target.constructor.name} and adding getter for the ${propertyKey} property`);
+
+        let element: HTMLElement;
+
         const getter = function () {
-            const element = document.querySelector(selector);
-            return element;
+            if (!element) {
+                element = document.querySelector(selector) as HTMLElement;
+                console.log(`Fetching DOM element with ${selector} selector to inject into ${propertyKey}`);
+            }
+            return element
         }
 
         Object.defineProperty(target, propertyKey, {
